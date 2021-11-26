d07efd081db5eb487c807cf3276acebe457df3aa
Hebert Wellington <hebertwellington@gmail.com>
2021-11-26 20:08:52 -0300

    H2 database, test profile, JPA

--- src/test/java/com/educandoweb/curso/CourseApplicationTests.java
+++ src/test/java/com/educandoweb/curso/CourseApplicationTests.java
@@ -1,10 +1,10 @@
 package com.educandoweb.curso;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CursoApplicationTests {
+class CourseApplicationTests {
 
 	@Test
 	void contextLoads() {
